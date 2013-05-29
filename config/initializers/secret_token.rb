584f3cbc21af17a38655132b9e4977e59a768ac2
Claron Twitchell <claront@gmail.com>
2013-05-28 22:36:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularCasts::Application.config.secret_token = 'fa855fc65a85893c6f3238b62ba39cbcd47e6526e46858983c1367c673e0d13fcf9732d259a73fed44d948f5d02568870054ab89c943c6609f78644e91afea22'
